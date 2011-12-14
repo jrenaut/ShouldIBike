16a43d4af1d28735a12bd253c6fcc9c61a785f2b
Jon Renaut <jon@manfredmacx.com>
2011-12-13 21:50:01 -0500

    go to one table

--- bikes.py
+++ bikes.py
@@ -4,7 +4,7 @@ render = web.template.render('templates/')
 
 urls = ('/', 'index',
         '/(\d+)', 'index')
-db = web.database(dbn='sqlite', db='/home/jrenaut/src/sqlite/bikes.db')
+db = web.database(dbn='sqlite', db='../../sqlite/flow.db')
 
 app = web.application(urls, globals())
 
@@ -14,8 +14,8 @@ class index:
         if q_id == None:
             q_id = 1
         limvar = dict(q_id=q_id)
-        question = db.select('question', limvar, where="id=$q_id")[0]        
-        answers = db.select('answer', limvar, where="p_question_id=$q_id")
+        question = db.select('node', limvar, where="id=$q_id")[0]        
+        answers = db.select('node', limvar, where="parent_id=$q_id")
         return render.index(question, answers)
         
 if __name__ == "__main__": app.run()
